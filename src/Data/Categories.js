574de36c24cb7cb1155846be3818d069bbcc128d
CHIRAG YADAV <erchirag12@gmail.com>
2024-08-05 22:32:23 +0530

    initial commit

---
+++ src/Data/Categories.js
@@ -0,0 +1,19 @@
+const Categories = [
+  {
+    category: "General Knowledge",
+    value: 9,
+  },
+  { category: "Science and Nature", value: 17 },
+  { category: "Computer", value: 18 },
+  { category: "Mathematics", value: 19 },
+  { category: "Mythology", value: 20 },
+  { category: "Sports", value: 21 },
+  { category: "Geography", value: 22 },
+  { category: "History", value: 23 },
+  { category: "Politics", value: 24 },
+  { category: "Animals", value: 27 },
+  { category: "Vehicles", value: 28 },
+  { category: "Gadgets", value: 30 },
+];
+
+export default Categories;
